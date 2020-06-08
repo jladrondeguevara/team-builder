ef5a0ceb8a82a32e9acf678d3132b7c609ad81ed
Jeovanni <jeovannildg@gmail.com>
2020-06-07 23:24:23 -0600

    started form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TeamList from './TeamList';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
       <TeamList/>
+      <Form/>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <form>
+            <label htmlFor="name">Add member</label>
+            <input id="fname"
+            type="text"
+            name="textfield"
+            placeholder="First name"
+            />
+        </form>
+    )
+};
+
+export default Form;

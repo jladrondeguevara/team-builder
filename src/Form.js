e7ad362f074c2180c7915cf394f0e018f4ed34cb
Jeovanni <jeovannildg@gmail.com>
2020-06-07 23:47:19 -0600

    form completed

--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,32 @@
 import React from "react";
+import './Form.css'
 
 const Form = () => {
     return (
-        <form>
-            <label htmlFor="name">Add member</label>
-            <input id="fname"
-            type="text"
-            name="textfield"
-            placeholder="First name"
-            />
-        </form>
+        <div className="formDiv">
+            <form>
+                <h3>Add member!</h3>
+                <label htmlFor="fname">First name</label>
+                <input id="fname"
+                type="text"
+                name="textfield"
+                placeholder="First name"
+                />
+                <label htmlFor="lname">Last name</label>
+                <input id="lname"
+                type="text"
+                name="textfield"
+                placeholder="Last name"
+                />
+                <label htmlFor="email">E-mail</label>
+                <input id="email"
+                type="text"
+                name="textfield"
+                placeholder="E-mail"
+                />
+            </form>
+            <button>Submit</button>
+        </div>
     )
 };
 

37f6082e30e5d6e1716aa2ebe03ef3d3a1436df3
Jeovanni <jeovannildg@gmail.com>
2020-06-09 22:23:42 -0600

    added styles

--- src/TeamList.js
+++ src/TeamList.js
@@ -1,20 +1,12 @@
 import React, {useState} from 'react';
 import "./TeamList.css";
 
-// const members = [
-//     {fname: "Jeovanni", email: "jeovannildg@gmail.com", role: "CEO"},
-//     {fname: "Sam", email: "sdif@gmail.com", role: "Lead"},
-//     {fname: "Rick", email: "dsjfha@gmail.com", role: "Engineer"},
-//     {fname: "Ryan", email: "lfadknf@gmail.com", role: "Manager"}
-// ]
-
 const TeamList = (props) => {
     const [teamList, setTeamList] = useState(props.members); 
 
     return (
         <div className="TeamList">
             {props.members.map(member => (
-                // return <li key={member.fname}>{member.fname}<br></br>{member.email}<br></br>{member.role}</li>
                 <div className="member" key={member.id}>
                     <h2>{member.fname}</h2>
                     <p>{member.email}</p>
